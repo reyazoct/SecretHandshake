9e7fed94525d4d2ca97c6da4b9f301efc4c55e07
Reyaz Ahmad <reyazoct@gmail.com>
2019-01-17 23:34:06 +0530

    Add: Decimal Value '1'

--- secretHandshake.spec.js
+++ secretHandshake.spec.js
@@ -1,1 +1,7 @@
+import { secretHandshake } from './secretHandshake';
 
+describe('Secret Handshake', () => {
+    test('binary 1 (decimal 1) is a wink', () => {
+        expect(secretHandshake(1)).toEqual(['wink']);
+    });
+});
