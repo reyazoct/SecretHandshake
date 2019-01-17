79f49510844a520f9874954eac836cb7bb76469b
Reyaz Ahmad <reyazoct@gmail.com>
2019-01-18 00:41:06 +0530

    Add: Decimal Value '2'

--- secretHandshake.js
+++ secretHandshake.js
@@ -1,3 +1,11 @@
 export const secretHandshake = (decimalValue) => {
-    return ['wink'];
+    const listItem = ['wink','double blink'];
+    let binaryValue = decimalValue.toString(2);
+    const getItems = (accumulator, current, curIndex) => {
+        if (current === '1') accumulator.push(listItem[curIndex]);
+        return accumulator;
+    };
+    return binaryValue.split('')
+        .reverse()
+        .reduce(getItems,[]);
 };
--- secretHandshake.spec.js
+++ secretHandshake.spec.js
@@ -4,4 +4,8 @@ describe('Secret Handshake', () => {
     test('binary 1 (decimal 1) is a wink', () => {
         expect(secretHandshake(1)).toEqual(['wink']);
     });
+
+    test('binary 10 (decimal 2) is a double blink', () => {
+        expect(secretHandshake(2)).toEqual(['double blink']);
+    });
 });
