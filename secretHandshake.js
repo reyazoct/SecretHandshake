e62d653799739c80ca74e1e8b1b2c8e9c9702a2a
Reyaz Ahmad <reyazoct@gmail.com>
2019-01-18 01:18:33 +0530

    Modify: Refactor code

--- secretHandshake.js
+++ secretHandshake.js
@@ -1,20 +1,23 @@
 export const secretHandshake = (decimalValue) => {
-    const listItem = ['wink', 'double blink', 'close your eyes', 'jump'];
-
     if(isNaN(decimalValue))
         throw new Error('Handshake must be a number');
+
     let binaryValue = decimalValue.toString(2);
 
-    const getItems = (accumulator, current, curIndex) => {
-        if (current === '1') accumulator.push(listItem[curIndex]);
-        return accumulator;
-    };
     let returnValue = binaryValue.split('')
         .reverse()
         .reduce(getItems, []);
 
     if(binaryValue.length < 5)
         return returnValue;
+
     returnValue.pop();
     return returnValue.reverse();
+};
+
+const listItem = ['wink', 'double blink', 'close your eyes', 'jump'];
+
+const getItems = (accumulator, current, curIndex) => {
+    if (Number(current)) accumulator.push(listItem[curIndex]);
+    return accumulator;
 };
